9e23af63cb108c9af63ec7c2e8dbc2ae02c1b6b5
Ivan Milicevic <ivan.milicevic.zu@gmail.com>
2018-10-17 13:41:06 +0200

    reorganise project and add Login

---
+++ TVShows/TVShows/Application/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  TVShows
+//
+//  Created by Ivan Milicevic on 17/10/2018.
+//  Copyright © 2018 Ivan Milicevic. All rights reserved.
+//
+
+import Foundation
---
+++ TVShows/TVShows/Application/Initializers/Initializable.swift
@@ -0,0 +1,13 @@
+//
+//  Initializable.swift
+//  TVShows
+//
+//  Created by Ivan Milicevic on 17/10/2018.
+//  Copyright © 2018 Ivan Milicevic. All rights reserved.
+//
+
+import Foundation
+
+protocol Initializable: class {
+    func performInitialization()
+}
